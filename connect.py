89decd4008e9f9d0d2a7fdb13cc3b9e5239e5e99
Ellen319 <simulabo5@gmail.com>
2024-08-18 19:54:27 +1200

    Create connect.py

---
+++ connect.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+# These details are available on the first MySQL Workbench screen
+# Usually called 'Local Instance'
+dbuser = "root"  # PUT YOUR MySQL username here - usually root
+dbpass = "13152727"  # PUT YOUR PASSWORD HERE
+dbhost = "localhost"
+dbport = "3306"
+dbname = "BookSense"
+
+def getCursor():
+    connection = mysql.connector.connect(
+        user=dbuser,
+        password=dbpass,
+        host=dbhost,
+        database=dbname,
+        autocommit=True
+    )
+    dbconn = connection.cursor()
+    return connection, dbconn
