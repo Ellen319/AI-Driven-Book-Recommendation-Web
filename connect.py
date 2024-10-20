78c80e304da80417fbb2196ac2a5ac72bad65be2
Ellen319 <simulabo5@gmail.com>
2024-10-20 17:02:04 +1300

    Update connect.py

--- connect.py
+++ connect.py
@@ -3,7 +3,7 @@ import mysql.connector
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
 dbuser = "root"  # PUT YOUR MySQL username here - usually root
-dbpass = "13152727"  # PUT YOUR PASSWORD HERE
+dbpass = "131527"  # PUT YOUR PASSWORD HERE
 dbhost = "localhost"
 dbport = "3306"
 dbname = "BookSense"
